ea2a3db7011a471ca6528b4504f3aec66eb8d30b
Aryanks <46104761+Aryanks@users.noreply.github.com>
2018-12-24 18:56:05 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,14 @@
-#! /bin/bash
-echo "Hello World"
-<<<<<<< HEAD
-pwd 
-=======
->>>>>>> 75fd5136004272d790c19541fdc9dbb0b45e31af
+var http = require("http");
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
